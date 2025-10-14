13e93bac61df56cb042aca47c99905d7226e4a2b
H S <ennovyhs@gmail.com>
2025-10-14 05:38:54 +0000

    move to left corner (_for element <UserProfile>_)

--- src/components/app-shell.tsx
+++ src/components/app-shell.tsx
@@ -121,7 +121,7 @@ function UserProfile({ collapsed = false }: { collapsed?: boolean }) {
     const { user, isUserLoading } = useUser();
     const coachImage = PlaceHolderImages.find(p => p.id === 'coach');
 
-    if (isUserLoading) return null; // Or a skeleton loader
+    if (isUserLoading && !user) return null; 
     
     const displayName = user?.displayName || "Johnny";
     const photoURL = user?.photoURL || coachImage?.imageUrl;
@@ -216,8 +216,8 @@ function AppShellInternal({ children }: { children: React.ReactNode }) {
                     <Logo />
                 </div>
                  <div className="hidden md:flex items-center gap-4">
-                    <SidebarTrigger />
                     {isClient && <UserProfile />}
+                    <SidebarTrigger />
                 </div>
             </div>
             <div className="flex items-center gap-4">
